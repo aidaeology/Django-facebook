b172d9c26c4f9424b65c1232c2264f35da5eca09
Aida Tavakkolie <atavakkolie@saymedia.com>
2011-06-01 11:25:26 -0700

    changing permissions scope

--- django_facebook/canvas.py
+++ django_facebook/canvas.py
@@ -1,10 +1,7 @@
 from django.http import QueryDict
 from django_facebook import settings as facebook_settings
 
-
-
-
-def generate_oauth_url(scope='email,read_stream', extra_data=None):
+def generate_oauth_url(scope='email,publish_stream', extra_data=None):
     query_dict = QueryDict('', True)
     canvas_page = facebook_settings.FACEBOOK_CANVAS_PAGE
     query_dict.update(dict(client_id=facebook_settings.FACEBOOK_APP_ID, redirect_uri=canvas_page, scope=scope))
